577edc5057209535a0200ae37bca2be296e30e50
gnodar01 <gnodar01@gmail.com>
2015-04-05 07:24:33 -0400

    Set map center to center of USA

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 function initialize() {
 	var mapOptions = {
-		center: { lat: -34.397, lng: 150.644},
-		zoom: 8
+		center: { lat: 39.50, lng: -98.35},
+		zoom: 5
 	};
 	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
