052f2b30b541cb6242b8c0359a0030ced02deda6
gnodar01 <gnodar01@gmail.com>
2015-04-12 02:19:51 -0400

    Clean up map code

--- js/map.js
+++ js/map.js
@@ -10,31 +10,32 @@ function initialize() {
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
 
-// Takes city, geocodes it, gets the lat & lng coords, sets a marker on that location in the map, and runs the search SeatGeek function on those coords.
-function codeAddress(city) {
-  var address = city;
-  geocoder.geocode( { 'address': address}, function(results, status) {
+/* Takes city, geocodes it, gets the lat & lng coords, sets a marker on that location in the map,
+ and runs the search SeatGeek function on those coords. */
+var codeAddress = function (city) {
+  geocoder.geocode( {'address': city}, function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
-      // Retrieve lat and long coordinates from Geocode results to pass into SeatGeeks api call
-      var newLat = results[0].geometry.location.k, newLng = results[0].geometry.location.D;
-      // Run SeatGeek api based on geocoded lat&lng coords
-      searchSeatGeek(newLat,newLng);
+      resultsLocation = results[0].geometry.location
+      
+      cityLat = resultsLocation.lat();
+      cityLng = resultsLocation.lng();
 
       /* Centers and sets a marker in the map on the geocoded address inputed by the user.
-      	results[0] is set because the geocoded address may contain more than one possible 
-      	result. The first result has the highest probability of being correct, and there
-      	is usually no need to use the others. I could have it display all results and have the
-      	user choose the correct one, but that may be overkill, because the vast majority of the
-      	time the first result is correct*/
-      map.setCenter(results[0].geometry.location);
+      results[0] is set because the geocoded address may contain more than one possible 
+      result. The first result has the highest probability of being correct, and there
+      is usually no need to use the others. I could have it display all results and have the
+      user choose the correct one, but that may be overkill, because the vast majority of the
+      time the first result is correct*/
+      map.setCenter(resultsLocation);
       var marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location,
-          icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+        map: map,
+        position: resultsLocation,
+        icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
       });
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
+    searchSeatGeek(cityLat,cityLng);
   });
 }
 
