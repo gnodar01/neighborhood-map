b34cb2dc4f1da92e1c071f739a336efb214df3b5
gnodar01 <gnodar01@gmail.com>
2015-04-11 21:25:51 -0400

    Revert 60606f9..67b729d
    This rolls back to commit 60606f97c3cedba8d7da48cfb64925e264367f48.

--- js/koScript.js
+++ js/koScript.js
@@ -1,4 +1,6 @@
 function MyViewModel() {
+	var self = this;
+
 	this.cityVal = ko.observable();
 	/* When the button is clicked, the city that was
 	inputted is sent to the google maps geocoder to get the
@@ -7,8 +9,9 @@ function MyViewModel() {
 	this.runSearch = function() {
 		codeAddress(this.cityVal());
 	}
+	this.performers = ko.observableArray();
 }
 
-var currentView = new MyViewModel();
+var vm = new MyViewModel();
 
-ko.applyBindings(currentView);
+ko.applyBindings(vm);
